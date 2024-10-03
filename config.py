6b20889879b93281d32ebc59f1052001c65f6026
Misstooheavy <165894199+Misstooheavy@users.noreply.github.com>
2024-10-04 00:25:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("27592877", ""))
-API_HASH = getenv("b079acad3e397d8fa153216d6eea9232", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+API_ID = int(getenv("API_ID", "27592877"))
+API_HASH = getenv("API_HASH", "b079acad3e397d8fa153216d6eea9232")
+BOT_TOKEN = getenv("BOT_TOKEN", "7883818164:AAG-kLWLefe-B95xbVXNrPtLvt6-PUybK0Q")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6949673056").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+LOG_GROUP = getenv("LOG_GROUP", "1002379908212")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "1373866049"))

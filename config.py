ba63456ff4274d65158491a69a5dcc373d879643
Misstooheavy <165894199+Misstooheavy@users.noreply.github.com>
2024-10-04 00:06:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("27592877", ""))
+API_HASH = getenv("b079acad3e397d8fa153216d6eea9232", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 MONGO_DB = getenv("MONGO_DB", "")
